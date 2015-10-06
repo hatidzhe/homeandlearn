4eac4a3272e26681d0bf57ca63a568d6c047dc1d
Hatidzhe <yusufova.h@gmail.com>
2015-10-06 15:16:38 +0300

    chg:second class added

---
+++ Section06/src/com/hatidzhe/MyMethods.java
@@ -0,0 +1,26 @@
+package com.hatidzhe;
+
+
+public class MyMethods {
+
+    int total() {
+        int a_Value = 10 + 10;
+        return a_Value;
+
+    }
+
+    void print_text() {
+        System.out.println("Some text here");
+    }
+
+    int total(int aNumber) {
+        int a_Value = aNumber + 20;
+        return a_Value;
+    }
+
+    void print_text(String aString, int aVal) {
+        System.out.println(aString + aVal);
+
+    }
+
+}
