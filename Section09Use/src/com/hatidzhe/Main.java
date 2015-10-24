9e5457afd41a217ead66a107efacd657e931ad2c
Hatidzhe <yusufova.h@gmail.com>
2015-10-24 14:17:13 +0300

    chg: end version

--- Section09Use/src/com/hatidzhe/Main.java
+++ Section09Use/src/com/hatidzhe/Main.java
@@ -9,6 +9,17 @@ public class Main {
 
         String file_name = "home/hatidzhe/Desktop/test.txt";
 
+        try {
+            ReadFile file = new ReadFile(file_name);
+            String[] aryLines = file.OpenFile();
+            int i;
+            for (i = 0; i < aryLines.length; i++) {
+                System.out.println(aryLines[i]);
+            }
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
+
 
     }
 }
