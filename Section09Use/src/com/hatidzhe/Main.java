a503ff6355765e461053d603b6dd11c71ec06100
Hatidzhe <yusufova.h@gmail.com>
2015-10-24 13:56:30 +0300

    chg: the present version

--- Section09Use/src/com/hatidzhe/Main.java
+++ Section09Use/src/com/hatidzhe/Main.java
@@ -7,15 +7,8 @@ public class Main {
 
     public static void main(String[] args) throws IOException {
 
-        FileReader fr = new FileReader("test.txt");
-        BufferedReader br = new BufferedReader(fr);
+        String file_name = "home/hatidzhe/Desktop/test.txt";
 
-        String line1 = br.readLine();
-        System.out.println(line1);
-        String line2 = br.readLine();
-        System.out.println(line2);
-        String line3 = br.readLine();
-        System.out.println(line3);
-        br.close();
+
     }
 }
